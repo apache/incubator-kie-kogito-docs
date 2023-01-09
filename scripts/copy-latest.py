25943acad72a6c2f7d93315897c82e1ebceb058d
Ricardo Zanini <zanini@redhat.com>
2023-01-09 14:33:22 -0300

    Incorporating radtriste's review
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- scripts/copy-latest.py
+++ scripts/copy-latest.py
@@ -1,6 +1,10 @@
+# Script to copy over the latest "numbered" version to the build directory
+# Must be called AFTER the website being built by "npm run install-build"
+# It will create a new directory with the numbered version, e.g. 1.32.0.Final with the contents of the "latest" directory.
+# We do this way because the GH Pages can't have redirection/rewrite rules applied like suggested on Antora pages: https://docs.antora.org/antora/latest/playbook/urls-redirect-facility/
+# See JIRA: https://issues.redhat.com/browse/KOGITO-8408
 import re
 import shutil
-import os
 from pathlib import Path
 
 pattern = re.compile("(data-version=\")((\d+\.)(\d+\.)(\*|\d+)\.Final)")
